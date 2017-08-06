da8f08aa03825e2f1769f7bfb23a1ea6e5fb9d10
asr2230 <asr2230@cs.rit.edu>
2017-08-06 16:26:26 -0400

    Completed the project.

--- NewMedicalLetter/AppDelegate.swift
+++ NewMedicalLetter/AppDelegate.swift
@@ -41,12 +41,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         // Saves changes in the application's managed object context before the application terminates.
-        self.saveContext()
+        
+        
+        DatabaseController.saveContext()
     }
 
     // MARK: - Core Data stack
 
-    lazy var persistentContainer: NSPersistentContainer = {
+    /*lazy var persistentContainer: NSPersistentContainer = {
         /*
          The persistent container for the application. This implementation
          creates and returns a container, having loaded the store for the
@@ -87,7 +89,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
             }
         }
-    }
+    }*/
 
 }
 
---
+++ NewMedicalLetter/Structs.swift
@@ -0,0 +1,21 @@
+//
+//  Structs.swift
+//  NewMedicalLetter
+//
+//  Created by Alden Reed on 7/21/17.
+//  Copyright © 2017 Alden Reed. All rights reserved.
+//
+
+import Foundation
+
+struct PatientEntries{
+    static var patients: [Patient] = []
+}
+
+struct Letter{
+    static var letter = ""
+}
+
+struct Index{
+    static var index: Int = 0
+}
